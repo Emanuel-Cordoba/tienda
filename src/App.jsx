a82f53c147e10b20614484da6d95d25b68dd3297
Emanuel <emanuelcordoba002Qgmail.com>
2024-06-21 19:42:18 -0300

    ticket

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,44 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
 import { ProductList } from './components/ProductList';
+import Ticket from './components/ticket'; // Asegúrate de que la ruta es correcta
 
 function App() {
-	const [allProducts, setAllProducts] = useState([]);
-	const [total, setTotal] = useState(0);
-	const [countProducts, setCountProducts] = useState(0);
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
 
-	return (
-		<>
-			<Header
-				allProducts={allProducts}
-				setAllProducts={setAllProducts}
-				total={total}
-				setTotal={setTotal}
-				countProducts={countProducts}
-				setCountProducts={setCountProducts}
-			/>
-			<ProductList
-				allProducts={allProducts}
-				setAllProducts={setAllProducts}
-				total={total}
-				setTotal={setTotal}
-				countProducts={countProducts}
-				setCountProducts={setCountProducts}
-			/>
-		</>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header
+                allProducts={allProducts}
+                setAllProducts={setAllProducts}
+                total={total}
+                setTotal={setTotal}
+                countProducts={countProducts}
+                setCountProducts={setCountProducts}
+              />
+              <ProductList
+                allProducts={allProducts}
+                setAllProducts={setAllProducts}
+                total={total}
+                setTotal={setTotal}
+                countProducts={countProducts}
+                setCountProducts={setCountProducts}
+              />
+            </>
+          }
+        />
+        <Route path="/ticket" element={<Ticket />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/ticket.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Ticket = () => {
+  return (
+    <div>
+      <h1>Compra Confirmada</h1>
+      <p>Gracias por tu compra.</p>
+    </div>
+  );
+};
+
+export default Ticket;
